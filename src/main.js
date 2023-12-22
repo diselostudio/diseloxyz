c95c86a30a2885991d39d0893a1263544554b055
diselostudio <diselo.studio@proton.me>
2023-12-22 15:32:38 +0000

    feat: accesibility & timezone interactions

--- src/main.js
+++ src/main.js
@@ -1,16 +1,45 @@
+let AccesibilityButton;
+let TimezoneButton;
+let ReducedMotionToggle;
+let HighContrastToggle;
+
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('DOMContentLoaded');
+  AccesibilityButton = document.querySelector('.interaction__accesibility');
+  TimezoneButton = document.querySelector('.interaction__timezone');
+  ReducedMotionToggle = document.querySelector('.interaction__reduced-motion');
+  HighContrastToggle = document.querySelector('.interaction__high-contrast');
+
+  AccesibilityButton.addEventListener('click', function () {
+    document.body.classList.toggle('accesibility-open');
+  })
+
+  TimezoneButton.addEventListener('click', function () {
+    document.body.classList.toggle('timezone-open');
+  })
+
+  ReducedMotionToggle.addEventListener('change', function (e) {
+    document.body.classList.toggle('reduced-motion-on', e.target.checked);
+  })
+
+  HighContrastToggle.addEventListener('change', function (e) {
+    document.body.classList.toggle('high-contrast-on', e.target.checked);
+  })
 })
 
 // On scroll start remove hashtag
 
 // On scoll change add 100px class
 
+// Styling on button interaction
 // Click accesibility add accesibility class
 // On load check if any accesibility option is enabled, if so, add classes and change input values
+// Accesibility toggles events
 
 // Click timezone add timezone class
+
 
 // On start begin timer counter and update clock interval
 
 // On document loaded and time interval began add ready class
+
+// Sketch
