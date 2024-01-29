cf03fc393276b7cb881a09edc30efd9a703e78ae
diselostudio <diselo.studio@proton.me>
2024-01-29 10:55:23 +0000

    feat: grabbing some analytics

--- src/scripts/analytics.js
+++ src/scripts/analytics.js
@@ -87,7 +87,47 @@ function getBrowser(userAgent) {
     return browser;
 }
 
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
 function sendEvent(params) {
-    console.log(params)
+
     if (import.meta.env.DEV) return;
+
+    const schema = {
+        sessionId: null,
+        event: null,
+        when: null,
+        experience: null,
+        location: null,
+        userAgent: null,
+        browser: null,
+        resolution: null,
+        element: null,
+        origin: window.location.href
+    }
+
+    const data = {
+        ...schema,
+        ...params,
+        when: formatDate(new Date(params.when)),
+        resolution: params.resolution ? Number(params.resolution.toFixed(2)) : null
+    };
+
+    fetch(
+        'https://api.tinybird.co/v0/events?name=site_events',
+        {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: { Authorization: 'Bearer p.eyJ1IjogImI3YmE1ZDFmLTcwMTEtNGU3OC04ZDM4LWVkODA3MzJjM2YyYiIsICJpZCI6ICIxOGUxZGMxYi1jMzgzLTRmMDQtYjVkMC1iYTMyMmY0OWFhNzgiLCAiaG9zdCI6ICJldV9zaGFyZWQifQ.jsTo2qvgUpnzTPU2Sr5y4hC8OaYkA2PeNcA-wwjV0jk' }
+        }
+    )
 }
